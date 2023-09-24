847174ecd41fafd474b87d3172818c36e70cdfb1
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-09-24 16:11:00 -0300

    feat: criado rota para consultar um cliente pelo id.

--- crm-api/src/main/java/com/edgard/crm/controller/ClienteController.java
+++ crm-api/src/main/java/com/edgard/crm/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +16,23 @@ import com.edgard.crm.model.Cliente;
 import com.edgard.crm.repository.ClienteRepository;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping(value="api")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping
+	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@PostMapping
+	@GetMapping("/clientes/{id}")
+	public Cliente listaClienteUnico(@PathVariable(value="id") long id) {
+		return clienteRepository.findById(id);
+	}
+	
+	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
--- crm-api/src/main/java/com/edgard/crm/repository/ClienteRepository.java
+++ crm-api/src/main/java/com/edgard/crm/repository/ClienteRepository.java
@@ -7,5 +7,6 @@ import com.edgard.crm.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+	
+	Cliente findById(long id);
 }
