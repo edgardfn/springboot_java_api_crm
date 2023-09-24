5335ca2eff155bf31e2f92f0e65976959ffd0d81
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-09-24 15:59:15 -0300

    feat: criado rota para adicionar novos clientes.

--- crm-api/src/main/java/com/edgard/crm/model/Cliente.java
+++ crm-api/src/main/java/com/edgard/crm/model/Cliente.java
@@ -7,9 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 @Entity
 public class Cliente {
 	
@@ -19,6 +19,39 @@ public class Cliente {
 	
 	@Column(nullable = false)
 	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
 
 	
 }
