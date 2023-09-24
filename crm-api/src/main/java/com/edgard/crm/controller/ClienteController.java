8b142484291d4622a097ecb7384ff47a9e0d9dcc
Edgard FInotti Neto <edgardfinotti@yahoo.com.br>
2023-09-24 16:25:26 -0300

    feat: criado rota para alterar os dados de um cliente.

--- crm-api/src/main/java/com/edgard/crm/controller/ClienteController.java
+++ crm-api/src/main/java/com/edgard/crm/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,5 +43,10 @@ public class ClienteController {
 	@DeleteMapping("/clientes")
 	public void deletarCliente(@RequestBody Cliente cliente) {
 		clienteRepository.delete(cliente);
+	}
+	
+	@PutMapping("/clientes")
+	public Cliente atualizarCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 }
